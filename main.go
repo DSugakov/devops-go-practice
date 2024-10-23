d143e9aaf4cedc57976c510434db7918e63a6d0c
Sugakov Denis <Sugakovdv@yandex.ru>
2024-10-23 23:03:58 +0300

    initial commit

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"devops-go-practice/alerts"
+	"devops-go-practice/metrics"
+	"devops-go-practice/polling"
+	"fmt"
+)
+
+const serverURL = "http://srv.msk01.gigacorp.local/_stats"
+const maxRetryCount = 3
+
+func main() {
+	pollServer := polling.InitiatePolling(serverURL, maxRetryCount)
+
+	for response := range pollServer() {
+		serverMetrics, err := metrics.ParseMetrics(response)
+		if err != nil {
+			fmt.Printf("Error parsing metrics: %v\n", err)
+			continue
+		}
+
+		alerts.CheckMetrics(serverMetrics)
+	}
+}
